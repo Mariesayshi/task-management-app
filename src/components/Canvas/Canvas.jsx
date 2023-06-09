74e18a9fbac348b9c885056738a6fcc017e436f3
Mariam Mdinaradze <marie.mdinaradze@gmail.com>
2023-06-09 17:09:22 +0400

    created socket groups

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -2,7 +2,130 @@ import { useEffect, useState } from "react";
 import { fabric } from "fabric";
 import classes from "./Canvas.module.css";
 
+const data = {
+  id: "1",
+  name: "MergeFiles",
+  sockets: [
+    {
+      id: "6",
+      name: "Merged_File",
+      type: "File",
+      io: "input",
+      process: "MRTW_LightBlue",
+    },
+    {
+      id: "7",
+      name: "Well_File",
+      type: "File",
+      io: "output",
+      process: "MRTW_DarkBlue",
+    },
+  ],
+  processes: [
+    {
+      id: "MRTW_LightBlue",
+      parentProcess: null,
+      color: "#ADD8E6",
+    },
+    {
+      id: "MRTW_DarkBlue",
+      parentProcess: "LightBlue",
+      color: "#00008B",
+    },
+  ],
+  links: [],
+  frontendOptions: {
+    id: "1",
+    positionInCanvas: [0, 0],
+  },
+};
+
+const createSocket = (socketText, processColor, top, type, io = true) => {
+  let socketBg = new fabric.Rect({
+    fill: "transparent",
+    width: 300,
+    height: 80,
+    left: 0,
+  });
+
+  let socketRect = new fabric.Rect({
+    width: 70,
+    height: 60,
+    fill: "white",
+    stroke: "black",
+    strokeWidth: 1,
+    top: 10,
+  });
+
+  socketRect.set({ left: io ? 10 : socketBg.width - socketRect.width - 10 });
+
+  let process = new fabric.Rect({
+    width: socketRect.width / 2,
+    height: 61,
+    fill: processColor,
+    originX: "left",
+    originY: "top",
+    top: 10,
+    left: socketRect.left - socketRect.width / 2,
+    strokeWidth: 0,
+  });
+
+  process.set({
+    left: io ? 10 + socketRect.width : socketRect.left - process.width,
+  });
+
+  let socketTitle = new fabric.Text(socketText, {
+    fontSize: 18,
+    fontFamily: "Roboto",
+    originX: "left",
+    originY: "top",
+    top: 5 + 5,
+  });
+
+  socketTitle.set({
+    left: io
+      ? 10 + 1.5 * socketRect.width + 10
+      : process.left - socketTitle.width - 10,
+  });
+
+  let socketType = new fabric.Text(type, {
+    fontSize: 14,
+    fontFamily: "Roboto",
+    originX: "left",
+    originY: "top",
+    top: (socketBg.height / 5) * 3,
+  });
+
+  socketType.set({
+    left: io ? socketTitle.left : process.left - socketType.width - 10,
+  });
+
+  return new fabric.Group(
+    [socketBg, process, socketRect, socketTitle, socketType],
+    {
+      top: top,
+    }
+  );
+};
+
+
+const createSocketsArr = (arr, inputOutput, io) => {
+  return arr
+    .filter((sckt) => sckt.io === inputOutput)
+    .map((sckt, i) => {
+      let processObj = data.processes.find((o) => o.id === sckt.process);
+      return createSocket(
+        sckt.name,
+        processObj.color,
+        0 + 80 * i,
+        sckt.type,
+        io
+      );
+    });
+};
+
 const Canvas = () => {
+  // ??? am fabricCanvas-ს არსად არ ვიყენებ?
   const [fabricCanvas, setFabricCanvas] = useState(null);
 
   useEffect(() => {
@@ -66,67 +189,34 @@ const Canvas = () => {
       strokeWidth: 0,
     });
 
-    let socketBg = new fabric.Rect({
-      fill: "transparent",
-      width: input.width,
-      height: 80,
-      left: 0,
+    let inputSocketsArr = createSocketsArr(data.sockets, "input");
+    let outputSocketsArr = createSocketsArr(data.sockets, "output", false);
+
+    const inputSocketGroup = new fabric.Group(inputSocketsArr, {
+      originY: "bottom",
+      top: subTaskBg.height,
     });
 
-    let socketRect = new fabric.Rect({
-      width: 70,
-      height: 60,
-      fill: "white",
-      stroke: "black",
-      strokeWidth: 1,
-      left: 10,
-      top: 10,
+    const outputSocketGroup = new fabric.Group(outputSocketsArr, {
+      originY: "bottom",
+      top: subTaskBg.height,
+      left: output.left,
     });
-
-    let process = new fabric.Rect({
-      width: socketRect.width / 2,
-      height: 61,
-      fill: "#12cdd4",
-      originX: "left",
-      originY: "top",
-      top: 10,
-      left: 10 + socketRect.width,
-      strokeWidth: 0,
-    });
-
-    let socketTitle = new fabric.Text("Socket #1", {
-      fontSize: 18,
-      fontFamily: "Roboto",
-      originX: "left",
-      originY: "top",
-      top: 5 + 5,
-      left: 10 + 1.5 * socketRect.width + 10,
-    });
-
-    let socketType = new fabric.Text("File", {
-      fontSize: 14,
-      fontFamily: "Roboto",
-      originX: "left",
-      originY: "top",
-      top: (socketBg.height / 5) * 3,
-      left: 10 + 1.5 * socketRect.width + 10,
-    });
-
-    let socket = new fabric.Group(
-      [socketBg, process, socketRect, socketTitle, socketType],
-      {
-        left: 5,
-        top: subTaskBg.height - socketBg.height,
-      }
-    );
 
     let subTask = new fabric.Group(
-      [subTaskBg, input, output, subTaskHeading, subTaskHeadingText, socket],
+      [
+        subTaskBg,
+        input,
+        output,
+        subTaskHeading,
+        subTaskHeadingText,
+        inputSocketGroup,
+        outputSocketGroup,
+      ],
       { left: 50, top: 10 }
     );
-    canvas.add(subTask);
 
-    // canvas.add(subTask);
+    canvas.add(subTask);
 
     // infinite Canvas panning
 
@@ -137,6 +227,8 @@ const Canvas = () => {
     });
 
     canvas.on("mouse:down", function (e) {
+      console.log(e.target);
+
       if (e.target === null) {
         panning = true;
       }
